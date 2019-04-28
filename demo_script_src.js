e1f4f94e1a80f59048b8a33ab36489059cfe5f54
Orlee Gillis <you@example.com>
2019-04-28 18:09:51 +0300

    bootstrap applied

---
+++ demo_script_src.js
@@ -0,0 +1,11 @@
+$('.image').click( function(event)) {
+ $(this).show();
+$(image2).hide();
+$(image3).hide();
+                  });
+$('.image2').click( function(event)) {
+  $(.image1).show();
+$(this).hide();
+$(image3).hide();
+
+});
